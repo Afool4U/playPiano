44e18a4e509b72f942714530e4b39f22ed00a17e
普信男 <2312163474@qq.com>
2022-12-06 23:04:29 +0800

    Update playPiano.py

--- playPiano/playPiano.py
+++ playPiano/playPiano.py
@@ -2,11 +2,13 @@
 """
 @author: Github-Afool4U
 @Description: playPiano.py
-@Date: 2022-12-5 12:07
+@Date: 2022-12-5 14:40
 
 """
 
+import os
 from os import environ
+from collections import OrderedDict
 import threading
 
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
@@ -17,6 +19,7 @@ import time
 class PlayPiano(threading.Thread):
     # 设置成类变量，保证所有实例共享同一个音频播放器
     pygame.mixer.init()
+    pygame.init()
     pygame.mixer.set_num_channels(1000)
     sounds = {}  # 音频缓存
     pitchs = ('_', '▁', '▂', '▃', '▄', '▅', '▆', '▇')  # 音高符号
@@ -32,16 +35,17 @@ class PlayPiano(threading.Thread):
         self.notes = None
         self.times = times
         self.notes_visible = notes_visible
+        self.ended = 0
 
-    def load_notes(self, file_path):
-        with open(file_path, 'r') as f:
-            self.notes = f.readlines()
-        self.notes = [note.split() for note in self.notes]
+    def load_notes(self, notes):
+        self.notes = notes
         return self
 
     def play(self):
         for notes in self.notes:
             for note in notes:
+                if self.ended:
+                    return
                 note = (note[1:] + note[0]).replace('+', 'h').replace('-', 'l')
                 if len(note) == 1:
                     note = 'm' + note
@@ -51,3 +55,49 @@ class PlayPiano(threading.Thread):
                 time.sleep(self.times / 1000)
             if self.notes_visible:
                 print()
+        self.ended = 1
+
+    def stop(self):
+        self.ended = 1
+
+    def __bool__(self):
+        return self.ended == 1
+
+
+class Notes:
+    def __init__(self):
+        self.notes = OrderedDict()
+
+    def load_note(self, file_path):
+        file = os.path.basename(file_path)
+        suffix = file.split('.')[1]
+        title = file.split('_')[0]
+        times = int(file[file.find('_') + 1:file.rfind('.')])
+        with open(file_path, 'r') as f:
+            notes = f.readlines()
+        notes = [note.split() for note in notes if '//' not in note]
+        if self.notes.get(title) is None:
+            self.notes[title] = {}
+        self.notes[title][suffix] = notes
+        self.notes[title]['times'] = times
+
+    def load_notes(self, path):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                self.load_note(os.path.join(root, file))
+        return self.notes
+
+
+class StopThreads(threading.Thread):
+    def __init__(self):
+        super().__init__(target=self.stop_threads)
+        self.threads = None
+        self.choice = None
+
+    def stop_threads(self):
+        line = input()
+        if any(self.threads):
+            self.choice = line
+        else:
+            for thread in self.threads:
+                thread.stop()
